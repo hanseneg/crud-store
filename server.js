44ec4f2a26424fccd54a1e5346ea6ea04fa0f5eb
Ethan Hansen <hanseneg@hotmail.com>
2021-02-22 11:34:19 -0700

    commit2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 app.use(express.json())
 app.use(morgan('dev'))
 
-mongoose.connect('mongodb://localhost:27017/bountiesdb',
+mongoose.connect('mongodb://localhost:27017/crudstoredb',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
